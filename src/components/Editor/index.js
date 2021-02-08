a0332778041c68ce88f7c0548b2eccadbe9551f7
intrvertmichael <michaelpaguay@gmail.com>
2021-02-08 17:58:23 -0500

    code highlighter completed

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,45 +1,109 @@
-import {useRef, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import './index.css'
 import './syntaxHighlighting.css'
 
 import regexReplacements from './regexReplacements'
 
 const Editor = props => {
-    const [outputValue, setOutputValue] = useState("")
+    const [inputValue, setInputValue] = useState()
+    const [outputValue, setOutputValue] = useState()
+    const [submitted, setSubmitted] = useState()
+    const [caretPos, setCaretPos] = useState()
     const textInput = useRef(null);
 
-    // tab feature still needs to be fixed
+    useEffect( () => {
+        const pos = caretPos + 3
+        textInput.current.setSelectionRange(pos,pos)
+    }, [textInput, caretPos])
+
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // prop defaults
+    const wordLimit = 200
+    const correctAnswer = "another answer"
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     const createMarkup = () => {
         return {__html: outputValue};
     }
 
+    const handleNextLevelClick = () => {
+        console.log("next level clicked")
+    }
+
     return (
         <div className="code-editor">
-            <div className="textarea-wrapper">
+            <div className="input-wrapper">
                 <textarea
                     ref={textInput}
                     className="input"
                     autoCorrect="off"
                     autoCapitalize="none"
                     spellCheck="false"
+                    maxLength={wordLimit}
                     placeholder="Enter code here"
+                    onChange={ e => setInputValue(e.target.value)}
                     onKeyDown={ e => {
                         if(e.keyCode === 9 || e.key === "Tab"){
                             e.preventDefault()
-                            const spaces = "   "
-                            const curPos = textInput.selectionStart
-                            const textValue = textInput.value
 
-                            textInput.value = textValue.slice(0,curPos) + spaces + textValue.slice(curPos)
-                            textInput.selectionEnd = curPos + 3
+                            const spaces = "   "
+                            const currentCaretPos = e.target.selectionStart
+                            setCaretPos(e.target.selectionStart)
+
+                            setInputValue(
+                                inputValue.slice(0,currentCaretPos)
+                                + spaces
+                                + inputValue.slice(currentCaretPos)
+                            )
                         }
                     }}
                     onKeyUp={ e => highlight(e, setOutputValue) }
+                    value={inputValue}
                 />
 
-                <div className="textarea-output" dangerouslySetInnerHTML={createMarkup()} />
+                <div className="textarea-output" dangerouslySetInnerHTML={createMarkup()}/>
+
+                <button
+                    className='submit-code'
+                    onClick = { e => {
+
+                        let result
+
+                        try {
+                            // eslint-disable-next-line no-eval
+                            result = global.eval(inputValue)
+                        }
+                        catch(error) {
+                            console.log(error)
+                        }
+
+                        if(result){
+                            setSubmitted( result )
+                        } else {
+                            setSubmitted("Please try again")
+                        }
+                    }}
+                > submit </button>
             </div>
+
+            {
+                submitted?
+                <div className="output-wrapper">
+                    {submitted}
+
+                    {
+                        submitted === correctAnswer ?
+                        <button onClick={handleNextLevelClick}> next level </button>
+                        :
+                        ""
+                    }
+                </div>
+                :
+                ""
+            }
+
         </div>
 )}
 
@@ -61,26 +125,31 @@ const highlight = (e, setOutputValue) => {
 
             if( (rep.name === "array" || rep.name === "function") && isArray){
 
-            // when trying to highlight array
-            // square brackets need to be escaped
+                // when trying to highlight array or function
+                // wrapper symbols need to be escaped
 
-            arr.forEach( found => {
-                const word = found.substring(1, found.length - 1)
-                const regex = new RegExp( '\\'+rep.code[0]+word+'\\'+rep.code[1]  , 'g')
-                const newElem = `<span class=${rep.name}>${found}</span>`
-                allText = allText.replace(regex , newElem) 
-            })
+                arr.forEach( found => {
+                    const strippedWord = found.substring(1, found.length - 1)
+                    const formattedRegex = '\\' + rep.code[0] + strippedWord + '\\' + rep.code[1]
 
-        } else if(isArray){
-            arr.forEach( found => {
-                const regex = new RegExp(`${found}`, 'g')
-                const newElem = `<span class=${rep.code}>${found}</span>`
-                allText = allText.replace(regex , newElem) 
-            })
+                    const createRegex = new RegExp( formattedRegex , 'g')
+                    const createNewElem = `<span class=${rep.name}>${found}</span>`
+
+                    allText = allText.replace(createRegex , createNewElem)
+                })
+
+            } else if(isArray){
+                arr.forEach( found => {
+                    const regex = new RegExp(`${found}`, 'g')
+                    const newElem = `<span class=${rep.code}>${found}</span>`
+                    allText = allText.replace(regex , newElem)
+                })
 
             }
         }  else {
+
             allText = allText.replace( rep.regex , rep.code)
+
         }
     })
 
