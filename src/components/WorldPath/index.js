53335c93e7c6abb802db31ab22a7ea086a8c2561
intrvertmichael <michaelpaguay@gmail.com>
2021-02-09 09:47:31 -0500

    path character updated

--- src/components/WorldPath/index.js
+++ src/components/WorldPath/index.js
@@ -3,7 +3,7 @@ import Castle from './castle.png'
 
 const WorldPath = props => {
 
-    const currentLevel = 3
+    const currentLevel = 2
 
     const onStyle = { background: '#BAA482' }
     const offStyle = { background: '#D69760' }
@@ -15,37 +15,33 @@ const WorldPath = props => {
     if( currentLevel === 1 ){
         onPathWidth = { width:"0%" }
         offPathWidth = { width:"100%" }
-        characterPos = { left:"calc(0% - 8px)" }
+        characterPos = { left:"calc(0% - 40px)" }
     } else if( currentLevel === 2 ){
         onPathWidth = { width:"25%" }
         offPathWidth = { width:"75%" }
-        characterPos = { left:"calc(25% - 16px)" }
+        characterPos = { left:"calc(25% - 40px)" }
     } else if( currentLevel === 3 ){
         onPathWidth = { width:"50%" }
         offPathWidth = { width:"50%" }
-        characterPos = { left:"calc(50% - 24px)" }
+        characterPos = { left:"calc(50% - 50px)" }
     } else if( currentLevel === 4 ){
         onPathWidth = { width:"75%" }
         offPathWidth = { width:"25%" }
-        characterPos = { left:"calc(75% - 32px)" }
+        characterPos = { left:"calc(75% - 50px)" }
     } else if( currentLevel === 5 ){
         onPathWidth = { width:"100%" }
         offPathWidth = { width:"0%" }
-        characterPos = { left:"calc(100% - 40px)" }
+        characterPos = { left:"calc(100% - 60px)" }
     }
 
     return (
         <div className='world-path nes-container is-dark with-title'>
 
-            <h1> path </h1>
-
             <div className='character-position'>
                 <img className='castle' src={Castle} alt="castle"/>
 
-                <img
-                    className='player'
-                    src={Castle}
-                    alt="player"
+                <i
+                    className="player nes-octocat animate"
                     style={characterPos}
                 />
 
