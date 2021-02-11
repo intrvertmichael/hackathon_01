d590df42c36bebc91c76f8f485980b897893ac34
intrvertmichael <michaelpaguay@gmail.com>
2021-02-11 09:12:24 -0500

    added coin to button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,49 @@
+
+import {useRef} from 'react'
 import './index.css'
 
 const Home = ({setCurrentPage}) => {
+
+    const coinDiv = useRef(null)
+
     return (
         <div className='home nes-container is-dark'>
             <h1>
-                <span className='nes-btn is-warning'>JS</span>
+                <button className='nes-btn is-warning' onClick={ e => {
+
+                    let animationOn
+                    const on = () => {
+                        animationOn = setTimeout( () => {
+                            coinDiv.current.classList.add('coin-animation')
+                        }, 10 )
+                    }
+
+                    let animationOff
+                    const off = () => {
+                        animationOff = setTimeout( () => {
+                            coinDiv.current.classList.remove('coin-animation')
+                        }, 1000 )
+                    }
+
+                    const clearOn = () => {
+                        clearTimeout(animationOn);
+                    }
+
+                    const clearOff = () => {
+                        clearTimeout(animationOff);
+                    }
+
+                    clearOn()
+                    clearOff()
+
+                    off()
+                    on()
+                    off()
+
+                }}>
+                    <div className='coin' ref={coinDiv} />
+                    JS
+                </button>
                 <span className='title'>World</span>
             </h1>
 
